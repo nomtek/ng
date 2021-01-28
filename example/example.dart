e84339ca1526d82e8a4c0c6e65b9a00604c57c9c
Zbigniew Gorawski <z.gorawski@nomtek.com>
2021-01-28 08:10:13 +0100

    removed main from example

--- example/example.dart
+++ example/example.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ng/ng.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
