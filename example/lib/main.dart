e70beb6c36ca139b3077b6ea01466bc8434f3272
Zbigniew Gorawski <z.gorawski@nomtek.com>
2021-01-28 10:09:26 +0100

    added whole project as an example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ng/ng.dart';
 
+void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
